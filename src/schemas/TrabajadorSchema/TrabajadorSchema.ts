d0db27f33065fbfe0878e313a78744e90177f357
iborra0306 <2866037@alu.murciaeduca.es>
2026-07-04 14:34:27 +0200

    Implementar tipos, mocks, esquemas de Zod y servicios CRUD para todos los modelos 2

--- src/schemas/TrabajadorSchema/TrabajadorSchema.ts
+++ src/schemas/TrabajadorSchema/TrabajadorSchema.ts
@@ -2,10 +2,18 @@
 import { z } from 'zod';
 
 export const trabajadorSchema = z.object({
-    tipo: z.string().optional(),
-    nombre: z.string().min(1, 'El nombre es obligatorio'),
-    apellido: z.string().optional(),
-    coste_hora_estandar: z.coerce.number().positive('El coste debe ser mayor que 0')
+    tipo: z
+        .string()
+        .optional(),
+    nombre: z
+        .string()
+        .min(1, 'El nombre es obligatorio'),
+    apellido: z
+        .string()
+        .optional(),
+    coste_hora_estandar: z
+        .coerce.number()
+        .positive('El coste debe ser mayor que 0')
 });
 
 // Evita duplicar interfaces en los formularios
